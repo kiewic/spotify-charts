d0f33179d1a4593c7a680c6f1d056967f9e4e0f5
Gilberto Stankiewicz <hello@kiewic.com>
2019-10-17 01:50:02 -0700

    Run scripts again to download charts that (for some unknown reason) were not available for download before (#1)

--- clean.js
+++ clean.js
@@ -5,18 +5,21 @@ const path = require('path');
 
 const files = fs.readdirSync('.');
 let filesWithErrors = 0;
+let filesWithoutErrors = 0;
 for (const fileName of files) {
     const fullFileName = path.join('.', fileName);
     if (fileName.match(/.csv$/)) {
         const content = fs.readFileSync(fullFileName, { encoding: 'utf8' });
         const hasError = content.substring(0, 50).match(/doctype html/);
         if (hasError) {
-            fs.renameSync(fullFileName, path.join('.', 'missing/', fileName));
+            fs.renameSync(fullFileName, path.join('./missing/', fileName));
+            filesWithErrors++;
         }
         else {
-            fs.renameSync(fullFileName, path.join('.', 'regional/', fileName));
+            fs.renameSync(fullFileName, path.join('./regional/', fileName));
+            filesWithoutErrors++;
         }
-        filesWithErrors++;
     }
 }
 console.log('filesWithErrors', filesWithErrors);
+console.log('filesWithoutErrors', filesWithoutErrors);
